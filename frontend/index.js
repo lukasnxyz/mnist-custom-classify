70723ac1a930357e3e1d886f6175645a45eef88b
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-12 14:59:38 -0500

    orginizing project and starting on web frontend

---
+++ frontend/index.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+// make this not whole screen but just a small window
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
