60a659bfaaa19831e475dd16776f90f9108876a2
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-12 14:39:47 -0500

    run in tkinter works, now going to build web front end

--- run.py
+++ run.py
@@ -9,48 +9,60 @@ import PIL
 WIDTH, HEIGHT = 400, 400
 CENTER = WIDTH // 2
 BLACK = (0, 0, 0)
-MODEL_PATH = "models/MNIST_model.pth"
-
-device = "cuda" if torch.cuda.is_available() else "cpu" # change this to mps
+MODEL_PATH = 'models/MNIST_model_revised.pth'
 model_modified = False
+
+d_opts = [('cuda', torch.cuda.is_available()), ('mps', torch.backends.mps.is_available()), ('cpu', True)]
+device = next(device for device, available in d_opts if available)
+print(f'using device: {device}')
 
 def img_to_arr(img_path: str) -> np.ndarray:
     with PIL.Image.open(img_path) as img:
-        img = img.convert("RGB").resize((28, 28))
+        img = img.convert('RGB').resize((28, 28))
         rgb_arr = np.array(img)
         grayscale_arr = 0.299 * rgb_arr[:, :, 0] + 0.587 * rgb_arr[:, :, 1] + 0.114 * rgb_arr[:, :, 2]
         grayscale_arr = grayscale_arr.astype(np.uint8)
     return grayscale_arr.flatten()
 
-# going to change model anyway
-class MNIST(nn.Module):
-    def __init__(self):
+class mnist_model(nn.Module):
+    def __init__(self, n_in: int, n_hidden: int, n_out: int):
         super().__init__()
-        self.h1 = nn.Linear(784, 128)
-        self.act1 = nn.ReLU()
-        self.output = nn.Linear(128, 10)
-        self.act_output = nn.Sigmoid()
+        self.block1 = nn.Sequential(
+            nn.Linear(n_in, n_hidden),
+            nn.ReLU(),
+            nn.BatchNorm1d(n_hidden),
+        )
+        self.block2 = nn.Sequential(
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.BatchNorm1d(n_hidden),
+        )
+        self.block3 = nn.Sequential(
+            nn.Linear(n_hidden, n_out),
+            nn.Sigmoid(),
+        )
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
-        X = self.act1(self.h1(X))
-        X = self.act_output(self.output(X))
-        return X
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        return x
 
 class PaintGUI:
     def __init__(self, model):
         self.model = model
 
         self.root = Tk()
-        self.root.title("Number Classifier")
+        self.root.title('Number Classifier')
 
         self.brush_width = 5
-        self.current_color = "#ffffff"
+        self.current_color = '#ffffff'
 
-        self.cnv = Canvas(self.root, width=WIDTH-10, height=HEIGHT-10, bg="black")
+        self.cnv = Canvas(self.root, width=WIDTH-10, height=HEIGHT-10, bg='black')
         self.cnv.pack()
-        self.cnv.bind("<B1-Motion>", self.paint)
+        self.cnv.bind('<B1-Motion>', self.paint)
 
-        self.image = PIL.Image.new("RGB", (WIDTH, HEIGHT), BLACK)
+        self.image = PIL.Image.new('RGB', (WIDTH, HEIGHT), BLACK)
         self.draw = ImageDraw.Draw(self.image)
 
         self.btn_frame = Frame(self.root)
@@ -58,16 +70,16 @@ class PaintGUI:
 
         self.btn_frame.columnconfigure(0, weight=1)
 
-        self.clear_btn = Button(self.btn_frame, text="Clear", command=self.clear)
+        self.clear_btn = Button(self.btn_frame, text='Clear', command=self.clear)
         self.clear_btn.grid(row=1, column=0, sticky=W+E)
 
-        self.classify_btn = Button(self.btn_frame, text="Classify", command=self.classify)
+        self.classify_btn = Button(self.btn_frame, text='Classify', command=self.classify)
         self.classify_btn.grid(row=2, column=0, sticky=W+E)
 
-        self.classify_btn = Button(self.btn_frame, text="Train", command=self.train)
+        self.classify_btn = Button(self.btn_frame, text='Train', command=self.train)
         self.classify_btn.grid(row=3, column=0, sticky=W+E)
 
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root.protocol('WM_DELETE_WINDOW', self.on_closing)
         self.root.mainloop()
 
     def paint(self, event):
@@ -87,15 +99,16 @@ class PaintGUI:
         )
 
     def _save(self):
-        filename = "num.png"
-        if filename != "":
+        filename = 'num.png'
+        if filename != '':
             self.image.save(filename)
 
     def _save_to_arr(self) -> torch.Tensor:
         self._save()
-        arr = img_to_arr("num.png")
+        arr = img_to_arr('num.png')
         arr = arr/arr.max()
         arr = torch.from_numpy(arr).type(torch.float).to(device)
+        arr = arr.unsqueeze(dim=0)
         return arr
 
     def classify(self):
@@ -103,9 +116,11 @@ class PaintGUI:
 
         self.model.eval()
         with torch.inference_mode():
-            pred = self.model(arr).numpy()
+            pred = self.model(arr)
+            pred = torch.softmax(pred, dim=1)
             n = pred.argmax()
-            print(f"=> {n}, probability: {pred[n] * 100:.2f}%")
+            pred = pred.cpu().numpy()
+            print(f'=> {n}, probability: {pred[0][n] * 100:.2f}%')
 
     def _train_single_step(self, arr: torch.Tensor, label: torch.Tensor):
         label = torch.from_numpy(label).to(device)
@@ -124,29 +139,25 @@ class PaintGUI:
         model_modified = True
         arr = self._save_to_arr()
 
-        label = int(simpledialog.askstring("Label", "Label:"))
+        label = int(simpledialog.askstring('Label', 'Label:'))
         self._train_single_step(arr, np.array(label))
-        print(f"Trained network with 1 sample of class: \"{label}\"")
+        print(f'Trained network with 1 sample of class: \'{label}\'')
 
     def clear(self):
-        self.cnv.delete("all")
-        self.draw.rectangle([0, 0, 1000, 1000], fill="black")
+        self.cnv.delete('all')
+        self.draw.rectangle([0, 0, 1000, 1000], fill='black')
 
     def on_closing(self):
         if model_modified:
-            print(f"Saving model to: {MODEL_PATH}")
+            print(f'Saving model to: {MODEL_PATH}')
             from pathlib import Path
             torch.save(obj=self.model.state_dict(),
                        f=MODEL_PATH)
-        print("Closing window")
+        print('Closing window')
         self.root.destroy()
 
-def main():
-    print(f"Using device: \"{device}\"")
-    model = MNIST().to(device)
-    model.load_state_dict(torch.load(f=MODEL_PATH, map_location=device))
+if __name__ == '__main__':
+    m = mnist_model(n_in=784, n_hidden=128, n_out=10).to(device)
+    m.load_state_dict(torch.load(f=MODEL_PATH, map_location=device))
 
-    PaintGUI(model)
-
-if __name__ == "__main__":
-  main()
+    PaintGUI(m)
