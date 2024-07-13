8302540e3791d20161ede00418e997e4a43a6644
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-12 21:16:19 -0500

    some things working

--- app/backend.py
+++ app/backend.py
@@ -3,29 +3,32 @@ from PIL import Image
 import io
 import base64
 import torch
-from model.model import mnist_model
+import torchvision.transforms as transforms
+import numpy as np
+from model import mnist_model
 
 app = Flask(__name__)
 model = mnist_model(784, 128, 10)
 model.load_state_dict(torch.load('../models/MNIST_model_revised.pth'))
 model.eval()
 
-def preprocess_image(image):
-    ## Implement preprocessing here (e.g., resizing, normalizing)
-    return image
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
+def preprocess_image(image):
+    resize = transforms.Resize([28, 28])
+    image = resize(image)
+    image = torch.from_numpy(np.array(image))
+    #image = image[:, :, 0]
+    return image
+
 @app.route('/classify', methods=['POST'])
 def classify():
     data = request.get_json()
-    print(data)
-    #image_data = data['image'].split(",")[1]
-    #image = Image.open(io.BytesIO(base64.b64decode(image_data)))
-    #image = preprocess_image(image)
-    #image_tensor = torch.tensor(image).unsqueeze(0)
+    image_data = data['image'].split(",")[1]
+    image = Image.open(io.BytesIO(base64.b64decode(image_data)))
+    image = preprocess_image(image)
     #with torch.no_grad():
     #    output = model(image_tensor)
     #    _, predicted = torch.max(output, 1)
